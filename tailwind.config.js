2cf9c72fd7756d7fd0702bcc8e39bb946b250307
masud2005 <masud20012005@gmail.com>
2024-08-07 16:36:52 +0600

    Navbar and Banner Section Added

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    container: {
+      container: "1200px"
+    },
+    // colors: {
+    //   primary: "#0092E4"
+    // }
+  },
+  plugins: [],
+}
